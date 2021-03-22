5502d062c1ae6cdd71a4b33b96b169b9e59fd840
DiegoFernandezC <67343574+DiegoFernandezC@users.noreply.github.com>
2021-03-22 10:17:09 -0300

    Add files via upload
    Primera versión, logra comunicación, cierto retraso en RX.

---
+++ receiver.ino
@@ -0,0 +1,84 @@
+// Feather9x_RX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <Servo.h>
+ 
+Servo myservo;
+
+/* for Feather32u4 RFM9x */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+
+
+// Set frequency
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  
+  myservo.attach(12);
+   
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  delay(100);
+
+  //reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(5, false);
+}
+
+void loop() {
+  if (rf95.available()) {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    if (rf95.recv(buf, &len)){
+      int valor = atoi((char *)buf);   //CHAR to INT.
+      myservo.write(valor);
+      digitalWrite(LED, HIGH);
+      Serial.print("Received: ");
+      Serial.println(valor);
+       Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+
+      // Send a reply
+      uint8_t data[] = "Dato recibido.";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED, LOW);
+    }
+  }
+}
---
+++ sender.ino
@@ -0,0 +1,87 @@
+// Feather9x_TX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+/* for feather32u4 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+
+int pin_pot = 18;    //Potentiometer pin.
+int valor_pot;       //Potentiometer value.
+
+// Set frequency.
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup() {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  analogReference(EXTERNAL);       //Set config for AREF pin.
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+
+  delay(100);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(5, false);
+}
+
+void loop() {
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting...");
+  
+  valor_pot = analogRead(pin_pot);
+  valor_pot = map(valor_pot, 0, 1023, 0, 255);
+  char radiopacket[3];
+  itoa(valor_pot, radiopacket, 10);              //INT to CHAR
+  Serial.print("Dato enviado: "); Serial.print(radiopacket[0]); Serial.print(radiopacket[1]); Serial.println(radiopacket[2]);  //Print send data.
+  //delay(10);
+  rf95.send((uint8_t *)radiopacket, 3);
+
+  Serial.println("Waiting for packet to complete..."); 
+  //delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(100)) { 
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+       Serial.println("Receive failed");
+  }    
+  else
+    Serial.println("No reply.");
+}
